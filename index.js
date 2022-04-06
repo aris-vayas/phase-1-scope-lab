9de71f1271a0e8350b3a038bed48525ac2a35230
Aris Vayas <aris.vayas@gmail.com>
2022-04-05 23:03:09 -0400

    commit tree bc3db343444b2b0bf37393a21e8e4880bd86adea

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+return customerName= customerName.toUpperCase()
+     
+}
+
+function  setBestCustomer(){
+     bestCustomer ='not bob'
+
+    return bestCustomer
+}
+function overwriteBestCustomer(){
+    bestCustomer= 'maybe bob'
+
+}
+const leastFavoriteCustomer = 'anyone'
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = "bob"
+    console.log(leastFavoriteCustomer)
+    return leastFavoriteCustomer
+
+}
+changeLeastFavoriteCustomer()
